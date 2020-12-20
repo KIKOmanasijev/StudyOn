71f924c45f3fe86bc00c4992f86b4bfb6e7f82ab
kikomanasijev <manasijevhristijan@gmail.com>
2020-12-20 12:33:19 +0100

    Sidemenu component

--- StudyOnFrontend/studyon/src/App.vue
+++ StudyOnFrontend/studyon/src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Sidemenu/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Sidemenu from "./components/Sidemenu";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { 
+    Sidemenu 
+  },
+  data(){
+    return {
+      courts: [
+        {
+          id: 1,
+          lat: 41.9990051,
+          lng: 21.2848064,
+          sport: "Football",
+          name: "Forza Karpos"
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
+html, body, * {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 </style>
---
+++ StudyOnFrontend/studyon/src/components/Sidemenu.vue
@@ -0,0 +1,86 @@
+<template lang="">
+    <div class="sidemenu">
+        <div class="logo">
+            <span>S</span>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li><i class="flaticon-map active"></i></li>
+                <li><i class="flaticon-football-game-field"></i></li>
+                <li><i class="flaticon-ask"></i></li>    
+            </ul>
+        </div>
+
+        <div class="user-profile">
+            <span>H</span>
+        </div>
+    </div>
+</template>
+
+<script>
+import('../assets/fonts/flaticon.css');
+
+export default {
+    name: "Sidemenu"
+}
+</script>
+
+<style>
+    .sidemenu {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        width: 100px;
+        height: 100%;
+        padding: 50px 15px;
+
+        box-shadow: 2px 0 15px rgba(0,0,0, .1);
+    }
+
+    .sidemenu .logo span {
+        font-size: 40px;
+        font-weight: 700;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 60px;
+        height: 60px;
+        color: #fff;
+        background: #685EFF;
+        border-radius: 50%;
+    }
+
+    .sidemenu .user-profile span {
+        font-size: 40px;
+        font-weight: 700;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 60px;
+        height: 60px;
+        color: #fff;;
+        background: #777;
+        border-radius: 50%;
+    }
+
+    .sidemenu .menu ul {
+        list-style: none;
+        padding-left: 0;
+    }
+
+    .sidemenu .menu li i{
+        font-size: 30px;
+        font-weight: 500;
+    }
+
+    .sidemenu .menu li i.active {
+        color: #685EFF;
+        font-weight: 600;
+    }
+
+    .sidemenu .menu li:not(:last-of-type){
+        margin-bottom: 40px;
+    }
+</style>
